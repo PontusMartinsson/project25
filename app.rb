e9c5a58214ba0fcd5f8a84265261b39d28bdc1a3
ntijoh-pontus-martinsson <pontus.martinsson@elev.ga.ntig.se>
2025-03-11 10:18:27 +0100

    lagt till parts

--- app.rb
+++ app.rb
@@ -1,7 +1,8 @@
 require 'sinatra'
+require 'sinatra/reloader'
+require 'sinatra/flash'
 require 'slim'
 require 'sqlite3'
-require 'sinatra/reloader'
 require 'bcrypt'
 
 require_relative 'models'
@@ -35,6 +36,53 @@ get '/project/show/:projectid' do
   slim :'project/show'
 end
 
-get '/project/build/:projectid' do
+get '/project/create' do
+  @parts = parts
+  slim :'project/create'
+end
 
+post '/project/new' do
+  parts = params.select { |item| item.start_with?('part') }.values
+  parts.map! { |item| item.to_i }
+  p parts
+  redirect '/project/create'
+end
+
+# /project/edit
+
+# get '/part/browse' do
+#   @result = parts
+#   slim :'part/browse'
+# end
+
+get '/part/browse' do # TRASIGT
+  @result = parts_search(flash[:name], flash[:type])
+  p @result
+  slim :'part/browse'
+end
+
+post '/part/search' do # TRASIGT
+  flash[:name] = params[:name]
+  flash[:type] = params[:type]
+  redirect '/part/browse'
+end
+
+get '/part/create' do
+  slim :'part/create'
+end
+
+post '/part/new' do
+  name = params[:name]
+  type = params[:type]
+
+  if verify_part_creation(name, type)
+    flash[:message] = 'That part already exists'
+    redirect '/part/create'
+  elsif name.empty? || type.empty?
+    flash[:message] = 'All fields must be filled in'
+    redirect '/part/create'
+  else
+    create_part(name, type)
+    redirect '/'
+  end
 end
--- models.rb
+++ models.rb
@@ -4,6 +4,10 @@ def connect_db
   db
 end
 
+def connect_db_nohash
+  SQLite3::Database.new('db/greja.db')
+end
+
 def projects_public
   db = connect_db
   db.execute("
@@ -18,7 +22,7 @@ end
 def projects_user(id)
   db = connect_db
   # § kolla om det är ens egna och i så fall spelar public ingen roll
-  db.execute("SELECT * FROM project WHERE userid = ? and public = 1", id)
+  db.execute("SELECT * FROM project WHERE userid = ? AND public = 1", id)
 end
 
 def projects_keyword(keyword)
@@ -28,11 +32,51 @@ def projects_keyword(keyword)
     FROM ((project_tag_rel 
       INNER JOIN project ON project_tag_rel.projectid = project.id) 
       INNER JOIN tag ON project_tag_rel.tagid = tag.id) 
-    WHERE public = 1 and projectname LIKE ?", "%#{keyword}%")
+    WHERE public = 1 AND projectname LIKE ?", "%#{keyword}%")
 end
 
 def project_id(id)
   db = connect_db
   # § kolla om det är ens egna och i så fall spelar public ingen roll
-  db.execute("SELECT * FROM project WHERE id = ? and public = 1", id)[0]
+  db.execute("SELECT * FROM project WHERE id = ? AND public = 1", id)[0]
+end
+
+def parts
+  db = connect_db
+  db.execute("SELECT * FROM part")
+end
+
+def parts_search(name, type) # TRASIGT
+  db = connect_db
+  name = '' if name.nil?
+  type = '' if type.nil?
+
+  if name.empty? && type.empty?
+    db.execute("SELECT * FROM part")
+  elsif name.empty?
+    db.execute("SELECT * FROM part WHERE name LIKE ?", name)
+  elsif type.empty?
+    db.execute("SELECT * FROM part WHERE type LIKE ?", type)
+  else
+    db.execute("SELECT * FROM part WHERE name LIKE ? AND type LIKE ?", [name, type])
+  end
+end
+
+def part_type(name)
+  db = connect_db_nohash
+  db.execute("SELECT type FROM part WHERE name = ?", name).flatten
+end
+
+def part_names
+  db = connect_db_nohash
+  db.execute("SELECT name FROM part").flatten
+end
+
+def create_part(name, type)
+  db = connect_db
+  db.execute("INSERT INTO part (name, type) VALUES (?, ?)", [name, type])
+end
+
+def verify_part_creation(name, type)
+  !(part_names.include?(name) && part_type(name).include?(type))
 end
