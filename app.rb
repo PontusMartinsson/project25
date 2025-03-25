a0aee12c0f7100e5424498c41366481d48b70610
ntijoh-pontus-martinsson <pontus.martinsson@elev.ga.ntig.se>
2025-03-25 08:33:29 +0100

    Ändra till restful

--- app.rb
+++ app.rb
@@ -13,74 +13,68 @@ get '/' do
   slim :tja
 end
 
-get '/project/browse' do
-  @result = projects_public
-  slim :'project/browse'
+get '/project' do
+  # @result = projects_public
+  @result = projects_search(flash[:name], flash[:creator], flash[:tag])
+  slim :'project/index'
 end
 
-get '/project/browse/search/:keyword' do
-  keyword = params[:keyword]
-  @result = projects_keyword(keyword)
-  slim :'project/browse'
+post '/project/search' do
+  flash[:name] = params['name']
+  flash[:creator] = params['creator']
+  flash[:tag] = params['tag']
+  redirect '/project'
 end
 
-get '/project/browse/user/:userid' do
+get '/project/user/:userid' do
   userid = params[:userid]
   @result = projects_user(userid)
-  slim :'project/browse'
+  slim :'project/index'
 end
 
-get '/project/show/:projectid' do
+get '/project/:projectid' do
   projectid = params[:projectid]
   @result = project_id(projectid)
   slim :'project/show'
 end
 
-get '/project/create' do
+get '/project/new' do
   @parts = parts
   slim :'project/create'
 end
 
-post '/project/new' do
+post '/project' do
   parts = params.select { |item| item.start_with?('part') }.values
   parts.map! { |item| item.to_i }
   p parts
-  redirect '/project/create'
+  redirect '/project/new'
 end
 
-# /project/edit
-
-# get '/part/browse' do
-#   @result = parts
-#   slim :'part/browse'
-# end
-
-get '/part/browse' do # TRASIGT
+get '/part' do
   @result = parts_search(flash[:name], flash[:type])
-  p @result
-  slim :'part/browse'
+  slim :'part/index'
 end
 
-post '/part/search' do # TRASIGT
-  flash[:name] = params[:name]
-  flash[:type] = params[:type]
-  redirect '/part/browse'
+post '/part/search' do
+  flash[:name] = params['name']
+  flash[:type] = params['type']
+  redirect '/part'
 end
 
-get '/part/create' do
+get '/part/new' do
   slim :'part/create'
 end
 
-post '/part/new' do
+post '/part' do
   name = params[:name]
   type = params[:type]
 
   if verify_part_creation(name, type)
     flash[:message] = 'That part already exists'
-    redirect '/part/create'
+    redirect '/part/new'
   elsif name.empty? || type.empty?
     flash[:message] = 'All fields must be filled in'
-    redirect '/part/create'
+    redirect '/part/new'
   else
     create_part(name, type)
     redirect '/'
--- models.rb
+++ models.rb
@@ -25,20 +25,70 @@ def projects_user(id)
   db.execute("SELECT * FROM project WHERE userid = ? AND public = 1", id)
 end
 
-def projects_keyword(keyword)
+# def projects_search(name, creator, tag)
+#   db = connect_db
+
+#   name = name.nil? || name.empty? ? '' : name.insert(0, '%') << '%'
+#   creator = creator.nil? || creator.empty? ? '' : creator.insert(0, '%') << '%'
+#   tag = tag.nil? || tag.empty? ? '' : tag.insert(0, '%') << '%'
+
+#   sql = "SELECT * FROM project"
+#   keywords = []
+
+#   unless name.empty? && creator.empty? && tag.empty?
+#     sql << " WHERE"
+#     sql, keywords = add_to_sql(sql, keywords, name, 'projectname')
+#     sql, keywords = add_to_sql(sql, keywords, creator, 'creator')
+#     sql, keywords = add_to_sql(sql, keywords, tag, 'tag')
+#   end
+
+#   db.execute(sql, keywords)
+# end
+
+# def add_to_sql(sql, keywords, var, column)
+#   unless var.empty?
+#     if sql[-1] == '?'
+#       sql << ' AND'
+#     end
+#     sql << " #{column} LIKE ?"
+#     keywords.append(var)
+#   end
+#   return sql, keywords
+# end
+
+def projects_search(name, creator, tag)
   db = connect_db
-  db.execute("
-    SELECT *
-    FROM ((project_tag_rel 
-      INNER JOIN project ON project_tag_rel.projectid = project.id) 
-      INNER JOIN tag ON project_tag_rel.tagid = tag.id) 
-    WHERE public = 1 AND projectname LIKE ?", "%#{keyword}%")
+
+  name = name.nil? || name.empty? ? '' : name.insert(0, '%') << '%'
+  creator = creator.nil? || creator.empty? ? '' : creator.insert(0, '%') << '%'
+  tag = tag.nil? || tag.empty? ? '' : tag.insert(0, '%') << '%'
+
+  sql = "SELECT * FROM project"
+  vars = []
+
+  unless name.empty? && creator.empty? && tag.empty?
+    sql << " WHERE"
+    unless name.empty?
+      sql << ' projectname LIKE ?'
+      vars.append(name)
+    end
+    unless creator.empty?
+      id = db.execute("SELECT id FROM user WHERE username LIKE ?", creator).first['id']
+      sql[-1] == '?' ? sql << ' AND userid = ?' : sql << ' userid = ?'
+      vars.append(id)
+    end
+    # unless tag.empty # hitta projekt med tag LIKE tag
+    # end
+  end
+
+  p sql, vars
+  db.execute(sql, vars)
 end
 
 def project_id(id)
   db = connect_db
   # § kolla om det är ens egna och i så fall spelar public ingen roll
-  db.execute("SELECT * FROM project WHERE id = ? AND public = 1", id)[0]
+  db.execute("SELECT * FROM project WHERE id = ? AND public = 1", id).first
 end
 
 def parts
@@ -46,17 +96,18 @@ def parts
   db.execute("SELECT * FROM part")
 end
 
-def parts_search(name, type) # TRASIGT
+def parts_search(name, type)
   db = connect_db
-  name = '' if name.nil?
-  type = '' if type.nil?
+
+  name = name.nil? || name.empty? ? '' : name.insert(0, '%') << '%'
+  type = type.nil? || type.empty? ? '' : type.insert(0, '%') << '%'
 
   if name.empty? && type.empty?
     db.execute("SELECT * FROM part")
   elsif name.empty?
-    db.execute("SELECT * FROM part WHERE name LIKE ?", name)
-  elsif type.empty?
     db.execute("SELECT * FROM part WHERE type LIKE ?", type)
+  elsif type.empty?
+    db.execute("SELECT * FROM part WHERE name LIKE ?", name)
   else
     db.execute("SELECT * FROM part WHERE name LIKE ? AND type LIKE ?", [name, type])
   end
